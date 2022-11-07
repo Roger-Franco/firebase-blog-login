5f97adfbf6c0d331af21dda8f7478dcbdc96fbcc
Roger-Franco <rogerfrancogalt@gmail.com>
2022-11-07 13:12:04 -0400

    Implementa o componente de criação de posts, estilização e implementa lógica para tornar rotas e elementos inascessíveis caso não esteja logado

--- README-install.md
+++ README-install.md
@@ -3,5 +3,5 @@ yarn add firebase
 yarn add react-router-dom
 Firebase: blog-login
 https://www.youtube.com/watch?v=zL0dKETbCNE&list=PLpPqplz6dKxUfvM22GXRvYA4s-mvJE0XF&index=5
-Time: 14:30
+Time: 52:00
 Build a Blog Website - React and Firebase Tutorial
--- src/pages/CreatePost.js
+++ src/pages/CreatePost.js
@@ -1,8 +1,46 @@
-import React from 'react'
+import { addDoc, collection } from 'firebase/firestore'
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { auth, db } from '../firebase'
 
-function CreatePost() {
+function CreatePost({ isAuth }) {
+  const [title, setTitle] = useState('')
+  const [postText, setPostText] = useState('')
+  let navigate = useNavigate()
+
+  const postsCollectionRef = collection(db, "posts")
+
+  const createPost = async () => {
+    console.log(auth, 'auth')
+    await addDoc(postsCollectionRef, {
+      title,
+      postText,
+      author: { name: auth.currentUser.displayName, id: auth.currentUser.uid }
+    })
+    navigate('/')
+  }
+
+  useEffect(() => {
+    if (!isAuth) {
+      navigate('/login')
+    }
+  }, [])
+
   return (
-    <div>CreatePost</div>
+    <div className="createPostPage">
+      <div className="cpContainer">
+        <h1>Create A Post</h1>
+        <div className="inputGp">
+          <label>Title:</label>
+          <input placeholder="Title..." onChange={(e) => setTitle(e.target.value)} />
+        </div>
+        <div className="inputGp">
+          <label>Post:</label>
+          <textarea placeholder="Post..." onChange={(e) => setPostText(e.target.value)} />
+        </div>
+        <button onClick={createPost}>Submit Post</button>
+      </div>
+    </div>
   )
 }
 
